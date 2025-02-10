7f30539c699869c6ef9f41953322b6739ca1d097
Willcas222 <88785542+Willcas222@users.noreply.github.com>
2025-02-10 00:36:58 -0500

    Update login.tsx

--- src/app/auth/sign-in/login.tsx
+++ src/app/auth/sign-in/login.tsx
@@ -30,7 +30,7 @@ export default function LoginForm() {
     setError(null);  
     setLoading(true); 
 
-    const url = "https://login-production-d568.up.railway.app/api/auth/login";
+    const url = "https://login-production-0754.up.railway.app/api/auth/login";
 
     try {
       const response = await fetch(url, {
