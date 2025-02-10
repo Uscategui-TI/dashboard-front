8bcfb39113680ac4c3b929a7e57e2a8eef72f5be
Willcas222 <88785542+Willcas222@users.noreply.github.com>
2025-02-09 23:54:43 -0500

    Update register.tsx

--- src/app/auth/sing-up/register.tsx
+++ src/app/auth/sing-up/register.tsx
@@ -30,7 +30,7 @@ interface RegisterData {
   
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      const url = "https://login-production-d568.up.railway.app/api/auth/register";
+      const url = "https://login-production-d568.up.railway.app/api/auth";
   
       try {
         const response = await fetch(url, {
