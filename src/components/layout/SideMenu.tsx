4e01d430e574006b9069360691f383b286ea2831
DIGITALSOFT-IT <duvandev01@gmail.com>
2025-02-22 16:39:24 -0500

    Update SideMenu.tsx

--- src/components/layout/SideMenu.tsx
+++ src/components/layout/SideMenu.tsx
@@ -39,7 +39,7 @@ export const SideMenu = () => {
             }
 
             try {
-                const response = await axios.get(`https://auth-service-production-40be.up.railway.app/api/auth/role`, {
+                const response = await axios.get(`https://auth-service-production-1a7a.up.railway.app/api/auth/role`, {
                     headers: { 'Authorization': `Bearer ${storedToken}` },
                     withCredentials: true,
                 });
