32d1cc6485da5eec5355ba3bb6b9595259f8d276
william <huila33@hotmail.com>
2025-03-10 00:49:58 -0500

    configuracion de axios

--- src/app/admin/number/number.tsx
+++ src/app/admin/number/number.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import axios from "axios";
 
 const apiWhatsApp = process.env.NEXT_PUBLIC_WHATSAPP_URL;
 
@@ -9,22 +9,21 @@ export default function PhoneNumberForm() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    try {
-      const response = await fetch(`${apiWhatsApp}/set-phone-number`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ phoneNumber }),
-      });
 
-      const data = await response.json();
-      console.log("Respuesta del servidor:", data);
-      
-      if (data.message) alert(data.message);
-      if (data.token) {
-        setLinkToken(data.token);
-        console.log("Token recibido:", data.token);
+    try {
+      const response = await axios.post(`${apiWhatsApp}/set-phone-number`, 
+        { phoneNumber }, 
+        { headers: { "Content-Type": "application/json" } }
+      );
+
+      console.log("Respuesta del servidor:", response.data);
+
+      if (response.data.message) alert(response.data.message);
+      if (response.data.token) {
+        setLinkToken(response.data.token);
+        console.log("Token recibido:", response.data.token);
       }
-      
+
     } catch (error) {
       console.error("Error al enviar el número:", error);
       alert("Hubo un error al guardar el número.");
