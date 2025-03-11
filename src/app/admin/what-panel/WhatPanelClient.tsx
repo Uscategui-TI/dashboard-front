e4fb20e85d4f40643b596359939e2ad530cd0f85
william <huila33@hotmail.com>
2025-03-11 13:45:44 -0500

    actualizacion final

--- src/app/admin/what-panel/WhatPanelClient.tsx
+++ src/app/admin/what-panel/WhatPanelClient.tsx
@@ -169,6 +169,10 @@ useEffect(() => {
                             <div className="col-span-6 sm:col-full">
                                 <button className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-primary-800" type="submit" onClick={cancelBroadcast}>Cancelar Difusión</button>
                             </div>
+                            <h1>mensajes</h1>
+                            {lastNumber && (
+                                <p>Último número enviado: <strong>{lastNumber}</strong></p>
+                            )}
                         </div>
                         <div>
                             <h1>mensajes</h1>
