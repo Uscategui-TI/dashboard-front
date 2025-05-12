3db527def7cec902df9a0b79da496913b175ded3
william <huila33@hotmail.com>
2025-05-12 00:37:36 -0500

    eliminacion de defaultvalue Exampleformone

--- src/components/form/example-form/ExampleFormOne.tsx
+++ src/components/form/example-form/ExampleFormOne.tsx
@@ -54,7 +54,6 @@ export default function ExampleFormOne() {
               options={options}
               placeholder="Select an option"
               onChange={handleSelectChange}
-              defaultValue=""
               className="bg-gray-50 dark:bg-gray-800"
             />
           </div>
